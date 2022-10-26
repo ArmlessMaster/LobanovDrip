756e557f9bd1f26f90599864b95c5328ef9f21bb
saddestnerd <andrey.dvugroshev@gmail.com>
2022-10-27 01:49:46 +0300

    Create Frontend

---
+++ frontend/src/components/routers/Routers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const useRoutes = () => {
+
+
+  return (
+      <Routes>
+          <Route path='/authorization'/>
+      </Routes>
+  )
+}
