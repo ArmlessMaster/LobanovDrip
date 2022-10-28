2ffa843f5018014614a6958cf0a09be0413c7774
deyasix <akikidzhan@gmail.com>
2022-10-28 08:46:20 +0300

    registration screen rework

--- mobile/App.js
+++ mobile/App.js
@@ -2,44 +2,43 @@ import React, { useEffect, useCallback } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import {Registration} from "./components/registration/Registration";
+import Registration from "./components/registration/Registration";
 
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   'vsr-osd-mono': require('./assets/fonts/VCR_OSD_MONO_1.001.ttf'),
-  //   'inter-medium': require('./assets/fonts/Inter-Medium.ttf'),
-  //   'roboto-light-i': require('./assets/fonts/Roboto-LightItalic.ttf'),
-  //   'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-  //
-  // });
-  //
-  // useEffect(() => {
-  //   async function prepare() {
-  //     await SplashScreen.preventAutoHideAsync();
-  //   }
-  //   prepare();
-  // }, []);
-  //
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
-  //
-  // if (!fontsLoaded) {
-  //   return null;
-  // }
+  const [fontsLoaded] = useFonts({
+          'vsr-osd-mono': require('./assets/fonts/VCR_OSD_MONO_1.001.ttf'),
+          'inter-medium': require('./assets/fonts/Inter-Medium.ttf'),
+          'roboto-light-i': require('./assets/fonts/Roboto-LightItalic.ttf'),
+          'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+          'VCR_OSD_MONO': require('./assets/fonts/VCR_OSD_MONO_1.001.ttf'),
+      });
 
-  return (
-      Registration()
-  );
+      useEffect(() => {
+          async function prepare() {
+              await SplashScreen.preventAutoHideAsync();
+          }
+          prepare();
+      }, []);
+
+      const onLayoutRootView = useCallback(async () => {
+          if (fontsLoaded) {
+              await SplashScreen.hideAsync();
+          }
+      }, [fontsLoaded]);
+
+      if (!fontsLoaded) {
+          return null;
+      }
+
+      return (
+          <View style={styles.container} onLayout={onLayoutRootView}>
+              <Registration/>
+          </View>
+      );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
--- mobile/components/registration/Registration.js
+++ mobile/components/registration/Registration.js
@@ -1,7 +1,5 @@
-import {Pressable, StyleSheet, Text, View} from "react-native";
-import React, {useCallback, useEffect} from 'react';
-import {useFonts} from "expo-font";
-import * as SplashScreen from 'expo-splash-screen';
+import {Pressable, StyleSheet, Text, TextInput, View} from "react-native";
+import React, {useState} from 'react';
 import ClothingCompanyJP from "../../assets/images/clothingCompanyJP.svg";
 import Kharkiv from "../../assets/images/KHARKIV.svg";
 import Scull from '../../assets/images/scull.svg';
@@ -9,22 +7,50 @@ import Christ from '../../assets/images/Christ.svg';
 import Arrow from '../../assets/images/Arrow 4.svg';
 import DecorJP from '../../assets/images/Decor Jpn.svg';
 import Decor from '../../assets/images/Registration Decor Thing.svg';
+import DecorBottom from '../../assets/images/Decor Thing.svg';
 
 
-export const Registration = () => {
-    const [fontsLoaded] = useFonts({
-        'VCR OSD Mono': require('../../assets/fonts/VCR_OSD_MONO_1.001.ttf'),
-    });
+export default function Registration() {
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
-
-    if (!fontsLoaded) {
-        return null;
+    function first() {
+        return (
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <Text style={styles.headerText}>IS THIS YOUR FIRST VISIT?</Text>
+                <Pressable style={styles.button} onPress={() => setFirst(false)}>
+                    <Text style={styles.buttonText}>CREATE AN ACCOUNT</Text>
+                </Pressable>
+                <Text style={styles.text}>
+                    <Text>
+                        Read the
+                    </Text>
+                    <Text style={{textDecorationLine: 'underline'}}> Privacy Policy, Rules and Site Selection
+                        Guidelines</Text>
+                </Text>
+            </View>
+        )
     }
+
+    function second() {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <TextInput style={styles.input} placeholder="EMAIL"
+                           placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} secureTextEntry={true} placeholder="PASSWORD"
+                           placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} secureTextEntry={true} placeholder="REPEAT PASSWORD"
+                           placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} placeholder="NAME"
+                           placeholderTextColor='white'></TextInput>
+                <TextInput style={styles.input} placeholder="SURNAME"
+                           placeholderTextColor='white'></TextInput>
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>SIGN-IN</Text>
+                </Pressable>
+            </View>
+        )
+    }
+
+    const [isFirst, setFirst] = useState(true)
     return (
         <View style={{flex: 1, backgroundColor: "#0D0D0D"}}>
             <View style={{backgroundColor: 'black', flex: 0.5}}></View>
@@ -44,63 +70,74 @@ export const Registration = () => {
                     <Text style={styles.loginText}>LOGIN-IN</Text>
                 </Pressable>
             </View>
-            <View style={{flex: 11, alignItems: 'center'}}>
+            <View style={{flex: 11}}>
                 <Kharkiv style={{position: 'absolute', right: 0, bottom: 0}}></Kharkiv>
                 <ClothingCompanyJP style={{position: 'absolute', left: '5%', bottom: '10%'}}></ClothingCompanyJP>
                 <Scull style={{position: 'absolute', top: '2%', left: '5%'}}></Scull>
                 <DecorJP style={{position: 'absolute', top: '3%', right: '5%'}}></DecorJP>
                 <Decor style={{position: 'absolute', right: '1%', top: '30%'}}>
                 </Decor>
-                <Text style={styles.headerText}>IS THIS YOUR FIRST VISIT?</Text>
-                <Pressable style={styles.button}>
-                    <Text style={styles.buttonText}>CREATE AN ACCOUNT</Text>
-                </Pressable>
-                <Text style={styles.text}>
-                    <Text>Read the </Text>
-                    <Text style={{textDecorationLine: 'underline'}}>Privacy Policy, Rules and Site Selection
-                        Guidelines</Text>
-                </Text>
+                <DecorBottom style={{position: 'absolute', bottom: '1%', alignSelf: 'center'}}></DecorBottom>
+                {
+                    isFirst ? (
+                        first()
+                    ) : second()
+                }
             </View>
             <View style={{flex: 1, backgroundColor: 'black'}}></View>
         </View>
     );
+
 }
 
 const styles = StyleSheet.create({
     button: {
+        height: '7%',
         width: '70%',
-        height: '5%',
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: '#DB2525',
+        top: '-5%'
     },
     buttonText: {
         color: "#FFFFFF",
         fontSize: 24,
-        fontFamily: "VCR OSD Mono",
+        fontFamily: "VCR_OSD_MONO",
     },
     text: {
         flex: 2,
         color: "#FFFFFF",
-        top: '5%',
         textAlign: "center",
         fontSize: 18,
-        fontFamily: "VCR OSD Mono",
+        fontFamily: "VCR_OSD_MONO",
         width: '85%'
     },
     headerText: {
         flex: 1,
-        top: '15%',
         height: 100,
         color: "#FFFFFF",
         fontSize: 44,
         textAlign: "center",
-        fontFamily: "VCR OSD Mono",
+        fontFamily: "VCR_OSD_MONO",
         shadowColor: 'rgba(0, 0, 0, 0.55)',
         shadowRadius: 4,
         shadowOffset: {width: 0, height: 4},
+        top: '10%'
     },
     loginText: {
-        flex: 1, left: '5%', color: 'white', fontSize: 18, fontFamily: 'VCR OSD Mono'
+        flex: 1, left: '5%', color: 'white', fontSize: 18, fontFamily: 'VCR_OSD_MONO'
+    },
+    input: {
+        height: '6%',
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: '3%',
+        backgroundColor:  "#1F1F1F",
+        textAlign: 'center',
+        fontFamily: 'VCR_OSD_MONO',
+        fontSize: 20,
+        color: 'white',
+        width: '70%',
+        top: '-15%'
     }
 });
